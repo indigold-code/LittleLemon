85e9bb0a4f103bcb87bd8526eee21ad1fd046b83
indigold-code <111799252+indigold-code@users.noreply.github.com>
2023-03-30 13:29:05 -0500

    added the first models to the database

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.IntegerField(primary_key=True, unique=True, validators=[MaxValueValidator(99999999999)],default=0)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    BookingDate = models.DateTimeField() 
+
+
+
+class Menu(models.Model):
+    MenuID = models.IntegerField(primary_key=True, unique=True, validators=[MaxValueValidator(99999)], default=0)
+    Title = models.CharField(max_length=255, unique=True)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(validators=[MaxValueValidator(99999)])
