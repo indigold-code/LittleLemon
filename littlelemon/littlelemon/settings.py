0e3ca3fb46d8e83e3f0dd0c99938b3f8c86853cc
indigold-code <111799252+indigold-code@users.noreply.github.com>
2023-04-12 15:34:41 -0500

    Update settings.py

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': '$lothGang420',
+        'PASSWORD': '************',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
